75505ab5a87d641e6ffd56275a2a46baa037b6b5
AndrewReiblein <andrew.reiblein@vulcanlabs.com>
2021-05-07 18:51:32 +0200

    user_controller and session_controller added

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,17 @@
+class SessionController <ApplicationController
+
+    def create
+        @user = User.find.by(username: params[:username])
+
+        if !!@user && @user.authenticate(params[;password])
+
+            session[:user_id] = @user.id
+            redirect_to user_path
+        else
+
+            message = 'Something went wrong! Make sure your username and password is correct'
+            redirect_to login_path, notice: message
+        end
+    end 
+end
+        
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'user/new'
+
   root 'posts#index'
+  
   resources :posts
+
+  get 'user/new'
+  get '/login', to: 'session#login'
+  post '/login', to: 'session#create'
+  post "/logout", to 'session#destroy'
+  get '/logout', to: 'session#destroy'
+  
 end
